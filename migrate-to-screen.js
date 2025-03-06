abffe4c46ef31103e6cc13c212d765a1f069abcd
Sang Nguyen <sangnguyen5697@gmail.com>
2025-03-06 16:25:55 +0700

    Update migrate-to-screen script to support method constants

--- migrate-to-screen.js
+++ migrate-to-screen.js
@@ -2,27 +2,73 @@ module.exports = function (fileInfo, api) {
   const j = api.jscodeshift;
   const root = j(fileInfo.source);
 
-  const methodRegexes = [
-    /^getBy/,
-    /^getAllBy/,
-    /^queryBy/,
-    /^queryAllBy/,
-    /^findBy/,
-    /^findAllBy/,
+  const methodsToMigrate = [
+    'findAllByAltText',
+    'findAllByDisplayValue',
+    'findAllByLabelText',
+    'findAllByPlaceholderText',
+    'findAllByRole',
+    'findAllByTestId',
+    'findAllByText',
+    'findAllByTitle',
+    'findByAltText',
+    'findByDisplayValue',
+    'findByLabelText',
+    'findByPlaceholderText',
+    'findByRole',
+    'findByTestId',
+    'findByText',
+    'findByTitle',
+    'getAllByAltText',
+    'getAllByDisplayValue',
+    'getAllByLabelText',
+    'getAllByPlaceholderText',
+    'getAllByRole',
+    'getAllByTestId',
+    'getAllByText',
+    'getAllByTitle',
+    'getByAltText',
+    'getByDisplayValue',
+    'getByLabelText',
+    'getByPlaceholderText',
+    'getByRole',
+    'getByTestId',
+    'getByText',
+    'getByTitle',
+    'queryAllByAltText',
+    'queryAllByDisplayValue',
+    'queryAllByLabelText',
+    'queryAllByPlaceholderText',
+    'queryAllByRole',
+    'queryAllByTestId',
+    'queryAllByText',
+    'queryAllByTitle',
+    'queryByAltText',
+    'queryByDisplayValue',
+    'queryByLabelText',
+    'queryByPlaceholderText',
+    'queryByRole',
+    'queryByTestId',
+    'queryByText',
+    'queryByTitle',
   ];
 
-  methodRegexes.forEach((regex) => {
+  let needScreenImport = false;
+
+  methodsToMigrate.forEach((methodName) => {
     root
-      .find(j.CallExpression)
-      .filter(
-        (path) =>
-          path.node.callee.type === 'Identifier' &&
-          path.node.callee.name.match(regex)
-      )
+      .find(j.CallExpression, {
+        callee: {
+          type: 'Identifier',
+          name: methodName,
+        },
+      })
       .replaceWith((path) => {
-        const screenIndentifier = j.identifier('screen');
+        needScreenImport = true;
+
+        const screenIdentifier = j.identifier('screen');
         const memberExpression = j.memberExpression(
-          screenIndentifier,
+          screenIdentifier,
           j.identifier(path.node.callee.name)
         );
 
@@ -46,7 +92,7 @@ module.exports = function (fileInfo, api) {
     })
     .forEach((path) => {
       const propertiesToRemove = path.node.id.properties.filter((property) =>
-        methodRegexes.some((regex) => property.key.name.match(regex))
+        methodsToMigrate.includes(property.key.name)
       );
 
       if (propertiesToRemove.length > 0) {
@@ -64,41 +110,43 @@ module.exports = function (fileInfo, api) {
       }
     });
 
-  // Ensure 'screen' is imported from '@testing-library/react'
-  const testingLibraryImport = root.find(j.ImportDeclaration, {
-    source: {
-      value: '@testing-library/react',
-    },
-  });
-
-  if (testingLibraryImport.size() > 0) {
-    // Import exists, check if 'screen' is imported
-    const screenImport = testingLibraryImport.find(j.ImportSpecifier, {
-      imported: {
-        name: 'screen',
+  if (needScreenImport) {
+    // Ensure 'screen' is imported from '@testing-library/react'
+    const testingLibraryImport = root.find(j.ImportDeclaration, {
+      source: {
+        value: '@testing-library/react',
       },
     });
 
-    if (screenImport.size() === 0) {
-      // 'screen' is not imported, add it
-      testingLibraryImport
-        .get()
-        .node.specifiers.push(j.importSpecifier(j.identifier('screen')));
-    }
-  } else {
-    // No existing import, create a new one
-    const newImport = j.importDeclaration(
-      [j.importSpecifier(j.identifier('screen'))],
-      j.literal('@testing-library/react')
-    );
+    if (testingLibraryImport.size() > 0) {
+      // Import exists, check if 'screen' is imported
+      const screenImport = testingLibraryImport.find(j.ImportSpecifier, {
+        imported: {
+          name: 'screen',
+        },
+      });
 
-    // find the first import statement, insert new import declaration before it
-    const firstImport = root.find(j.ImportDeclaration).at(0);
-    if (firstImport.size() > 0) {
-      firstImport.insertBefore(newImport);
+      if (screenImport.size() === 0) {
+        // 'screen' is not imported, add it
+        testingLibraryImport
+          .get()
+          .node.specifiers.push(j.importSpecifier(j.identifier('screen')));
+      }
     } else {
-      // No existing import statements, insert at the top of the file
-      root.get().node.body.unshift(newImport);
+      // No existing import, create a new one
+      const newImport = j.importDeclaration(
+        [j.importSpecifier(j.identifier('screen'))],
+        j.literal('@testing-library/react')
+      );
+
+      // find the first import statement, insert new import declaration before it
+      const firstImport = root.find(j.ImportDeclaration).at(0);
+      if (firstImport.size() > 0) {
+        firstImport.insertBefore(newImport);
+      } else {
+        // No existing import statements, insert at the top of the file
+        root.get().node.body.unshift(newImport);
+      }
     }
   }
 
